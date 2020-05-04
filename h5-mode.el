ee3384d5faae28b15e9d659d6487b0ef853deb0b
H. Joe Lee <hyoklee@hdfgroup.org>
2020-05-03 21:31:17 -0500

    Create h5-mode.el

---
+++ h5-mode.el
@@ -0,0 +1,10 @@
+;;; h5-mode.el --- major mode for editing HDF5 file
+
+;; Copyright (C) 2020 Hyokyung Lee
+;; Author: Hyokyung Lee
+;; Created: May 2020
+;; Version: 0.0.1
+;; Keywords: hdf5
+;; URL: https://github.com/hyoklee/h5-el
+;; Package-Requires: ((seq "2.20") (emacs "24.3"))
+
