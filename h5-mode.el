4d18a27c87e0c885796138a2218fe584db287e1a
H. Joe Lee <hyoklee@hdfgroup.org>
2020-11-10 17:51:27 -0600

    Update h5-mode.el

--- h5-mode.el
+++ h5-mode.el
@@ -8,3 +8,24 @@
 ;; URL: https://github.com/hyoklee/h5-el
 ;; Package-Requires: ((seq "2.20") (emacs "24.3"))
 
+(eval-when-compile (require 'cl-lib))
+(require 'arc-mode)
+(defgroup h5 nil
+  "Simple editing of h5 files."
+  :prefix "h5-"
+  :group 'data)
+
+(defvar h5-mode-map
+  (let ((map (make-keymap)))
+    (suppress-keymap map)
+    (define-key map " " 'h5-next-line)
+    (define-key map "C" 'h5-copy)))
+
+(define-derived-mode h5-mode special-mode "h5"
+  (and buffer-file-name
+       (file-writable-p buffer-file-name)
+       (setq buffer-read-only nil)))
+       
+;;;
+(put 'h5-mode 'mode-class 'special)
+(provide 'h5-mode)
